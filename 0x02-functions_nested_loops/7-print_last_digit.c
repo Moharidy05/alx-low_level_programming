7462ac58f5b766486c3a71d1fd181f64ba971220
moharidy <moharidy10@gmail.com>
2023-09-18 13:03:38 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -13,7 +13,7 @@ int print_last_digit(int n)
 	last = n % 10;
 
 	if (last < 0)
-[2;2R[>77;30604;0c]10;rgb:bfbf/bfbf/bfbf]11;rgb:0000/0000/0000		last = last * -1;
+		last = last * -1;
 
 	_putchar(last + '0');
 
